26f4c4dd3b28d88a0829f65189e1e262e4b285c2
ajayexperion <ajay.vs@experionglobal.com>
2023-01-03 17:31:42 +0530

    filter query change

--- ecommerce_backend/ecom_app/apis/product_review.py
+++ ecommerce_backend/ecom_app/apis/product_review.py
@@ -1,5 +1,5 @@
 from beanie import PydanticObjectId
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends
 from bson import ObjectId
 from ecommerce_database.models import productReview
 from ecom_app.apis. product_model import Products,ResponseModel
@@ -16,16 +16,22 @@ async def add_product(review:Products):
     return {"message":"Review added succesfull"}
          
 # API to get product details
-@router.get("/productlist",response_description="Review records retrieved")
-async def get_products():
-    reviews:list= await get_products_details()
-    print(reviews)
-    return reviews
+# @router.get("/productlist",response_description="Review records retrieved")
+# async def get_products():
+#     reviews:list= await get_products_details()
+#     print(reviews)
+#     return reviews
       
 # API to filter product 
-@router.get("/product/", response_description="Review records retrieved")
-async def get_product(minAmount:int=None,maxAmount:int=None,productName:str=Header(default=None))->Products:
 
+# async def get_header_values(productName: str = Depends(Header()), minAmount: str = Depends(Header())):
+#     print(productName)
+#     return productName,minAmount
+
+@router.get("/product/", response_description="Review records retrieved")
+
+async def get_product(minAmount:int=Header(default=None),maxAmount:int=Header(default=None),productName:str=Header(default=None))->Products:
+    
     reviews=await get_filter_products(productName,minAmount,maxAmount)
     return reviews
 
--- ecommerce_backend/ecommerce/product.py
+++ ecommerce_backend/ecommerce/product.py
@@ -23,16 +23,11 @@ async def get_products_details():
 async def get_filter_products(productName,minAmount,maxAmount):
 
     if (minAmount)and(maxAmount)and(productName):
-        print(minAmount)
-        print(productName)
         reviews=  await productReview.find({"productName":productName,"amount":{"$gte":minAmount,"$lte":maxAmount}}).to_list()
     elif productName:
-        reviews=  await productReview.find(
-            productReview.productName==
-            productName).to_list()
+        reviews=  await productReview.find({"productName":productName}).to_list()
     elif minAmount and maxAmount:
         reviews=  await productReview.find({"amount":{"$gte":minAmount,"$lte":maxAmount}}).to_list()
-            
     else:
         reviews = await productReview.find_all().to_list()
     return reviews
